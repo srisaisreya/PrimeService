f18d17d332c9c832ffe9e5049a4792daac29c10e
srisaisreya <156003439+srisaisreya@users.noreply.github.com>
2024-04-05 00:05:36 -0400

    Create AuthenticationDBRepository.java

---
+++ src/main/java/edu/iu/svarikot/svarikotprimeservice/repository/AuthenticationDBRepository.java
@@ -0,0 +1,10 @@
+package edu.iu.rokala.primesservice.repository;
+
+import edu.iu.rokala.primesservice.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthenticationDBRepository extends CrudRepository<Customer, String> {
+    Customer findByUsername(String username);
+}
