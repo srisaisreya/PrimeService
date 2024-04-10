cdec5358ee17ba212c822c32ccec68d8eb622ede
srisaisreya <156003439+srisaisreya@users.noreply.github.com>
2024-04-09 23:25:20 -0400

    added rabbitmq

---
+++ src/main/java/edu/iu/svarikot/svarikotprimeservice/rabbitmq/MQConfiguration.java
@@ -0,0 +1,15 @@
+package edu.iu.svarikot.svarikotprimeservice.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class MQConfiguration {
+    @Bean
+    public Queue queue() {
+        return new Queue("primes");
+    }
+}
